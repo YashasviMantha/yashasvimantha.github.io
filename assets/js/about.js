4c79110f750aacf99d82adf2f5794c7b3d9fa1d7
YashasviMantha <yashasvimantha@gmail.com>
2020-07-30 21:26:39 +0530

    PB link change, About me word list change

--- assets/js/about.js
+++ assets/js/about.js
@@ -5,17 +5,13 @@ const head_line = () => {
 
 async function change_headline() {
     var elem = document.getElementById("head_line");
-    var array = ["a chess Player", "a ML Researcher", "an amature writer", "a nerd", "an extrovert","a pirate","a prankster","a Mumbaikar","a chaat lover"];
+    var array = ["a chess enthusiast", "a ML Researcher", "an amature writer", "a Madrasi", "an extrovert", "a pirate", "single.  :p", "a Mumbaikar", "a chaat lover"];
     var i = 0;
-    while(true)
-    {
-        if(i == array.length)
-        {
-            i = 0;   
-        }
-        else
-        {
-            elem.innerText = "I am " + array[i]+".";
+    while (true) {
+        if (i == array.length) {
+            i = 0;
+        } else {
+            elem.innerText = "I am " + array[i] + ".";
             i = i + 1;
             await new Promise(r => setTimeout(r, 1000));
         }
