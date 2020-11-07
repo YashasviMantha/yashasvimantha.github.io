a2ad0de28cd03689cf0af8e3675d457e1e00a283
YashasviMantha <yashasvimantha@gmail.com>
2020-11-07 12:01:31 +0530

    alert message

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const onLoadFunction = () => {
     change_heading();
+    alert("Hi! Thanks for visiting. The site is still under active development so feedback would be highly appreciated. Now click on OK and hop in!");
 }
 
 function change_heading() {
