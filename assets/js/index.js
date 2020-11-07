ca8e51c161c22b97439e604516192000d5829cbb
YashasviMantha <yashasvimantha@gmail.com>
2020-11-07 11:45:13 +0530

    removed alert

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,5 @@
 const onLoadFunction = () => {
     change_heading();
-    alert("Hi! Thanks for visiting. The site is still under active development so feedback would be highly appreciated. Now click on OK and hop in!");
-
 }
 
 function change_heading() {
